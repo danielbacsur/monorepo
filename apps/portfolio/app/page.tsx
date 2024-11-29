cf427c4c3aaba78fc676c333a1fc15e0ce27f7ef
Daniel Bacsur <contact@danielbacsur.com>
2024-11-29 16:23:44 +0100

    feat: update sections with experiences and achievements

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -96,10 +96,13 @@ export default function Page() {
       <section className="px-4 py-3 overflow-hidden">
         <div className="max-w-2xl mx-auto">
           <p className="text-justify">
-            Childhood - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Morbi justo sem, maximus eu accumsan non, vestibulum sit amet orci.
-            Sed viverra mauris quis iaculis ultrices. Sed et libero ut velit
-            dapibus facilisis non sit amet augue.
+            Since childhood, I have been fascinated by physics and robotics.
+            During kindergarten, I became obsessed with disassembling various
+            everyday objects, and by the time I started elementary school, it
+            was clear that this would be my field of interest. It was then that
+            I met my early mentor, Zoltán Jarosievitz, and not long after, as a
+            second-grader, I won my first physics competition against
+            eighth-graders.
           </p>
         </div>
       </section>
@@ -124,10 +127,15 @@ export default function Page() {
       <section className="px-4 py-3 overflow-hidden">
         <div className="max-w-2xl mx-auto">
           <p className="text-justify">
-            Acting - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Morbi justo sem, maximus eu accumsan non, vestibulum sit amet orci.
-            Sed viverra mauris quis iaculis ultrices. Sed et libero ut velit
-            dapibus facilisis non sit amet augue.
+            Art, dance, and music have always been important to me. However, in
+            fifth grade, I had the opportunity to play the lead role in the
+            Hungarian State Opera House&apos;s production of Billy Elliot — the
+            largest children's role in the world — which marked a major turning
+            point in my life. For a little over three years, I trained more than
+            40 hours a week, learning dance, singing, guitar, and, not least,
+            performance techniques and speech skills. Looking back, I&apos;m
+            deeply grateful for this experience, as it continues to enrich my
+            life even today, shaping me into a better entrepreneur and person.
           </p>
         </div>
       </section>
@@ -152,10 +160,16 @@ export default function Page() {
       <section className="px-4 py-3 overflow-hidden">
         <div className="max-w-2xl mx-auto">
           <p className="text-justify">
-            Robocup - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Morbi justo sem, maximus eu accumsan non, vestibulum sit amet orci.
-            Sed viverra mauris quis iaculis ultrices. Sed et libero ut velit
-            dapibus facilisis non sit amet augue. Vestibulum quam ligula.
+            In the summer of 2021, the RoboCup World Championship took place,
+            attracting teams from all over the world. My team represented
+            Hungary in the OnStage category, where the goal was to deliver a
+            two-minute performance using robots designed and built by the
+            participants. We created a robotic band featuring a fully automated
+            piano robot and a drum robot, accompanied by a separate piano and an
+            electric guitar. After winning the national round in the spring, we
+            advanced to the international competition, where, after three days
+            of challenges, the jury unanimously awarded us the World
+            Championship title for Best Hardware in the OnStage category.
           </p>
         </div>
       </section>
@@ -180,11 +194,14 @@ export default function Page() {
       <section className="px-4 py-3 overflow-hidden">
         <div className="max-w-2xl mx-auto">
           <p className="text-justify">
-            Kaspok - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Morbi justo sem, maximus eu accumsan non, vestibulum sit amet orci.
-            Sed viverra mauris quis iaculis ultrices. Sed et libero ut velit
-            dapibus facilisis non sit amet augue. Vestibulum quam ligula,
-            fringilla id sapien sed, blandit fringilla mauris.
+            After the World Championship, I founded my first student
+            business—since, in compliance with Hungarian laws, I couldn't
+            establish a real LLC yet—in which my classmates and I managed
+            everything from production to the sale of our products. We encased
+            ceramic planters with abstract 3D silicone molds that I designed,
+            and then planted stabilized moss in them. As a result, after a month
+            or two of work, we exited the business with a profit of a few
+            thousand dollars.
           </p>
         </div>
       </section>
@@ -209,13 +226,13 @@ export default function Page() {
       <section className="px-4 py-3 overflow-hidden">
         <div className="max-w-2xl mx-auto">
           <p className="text-justify">
-            Cypher - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Morbi justo sem, maximus eu accumsan non, vestibulum sit amet orci.
-            Sed viverra mauris quis iaculis ultrices. Sed et libero ut velit
-            dapibus facilisis non sit amet augue. Vestibulum quam ligula,
-            fringilla id sapien sed, blandit fringilla mauris. Duis facilisis
-            luctus magna eget venenatis. Integer nec diam suscipit, pretium leo
-            vitae, mattis risus. Nunc efficitur ultrices placerat.
+            In 2023, a classmate and I submitted a proposal to the National
+            Science and Innovation Olympiad. For the competition, we developed a
+            credit card-sized computer capable of preventing a significant
+            portion of data thefts and supporting the work of cybersecurity
+            professionals. The device was able to verify, based on the presence
+            of nearby networks, whether the user was authorized to access the
+            contents of the built-in drive, thus preventing unauthorized access.
           </p>
         </div>
       </section>
@@ -240,9 +257,12 @@ export default function Page() {
       <section className="px-4 py-3 overflow-hidden">
         <div className="max-w-2xl mx-auto">
           <p className="text-justify">
-            Farfler - Fringilla id sapien sed, blandit fringilla mauris. Duis
-            facilisis luctus magna eget venenatis. Integer nec diam suscipit,
-            pretium leo vitae, mattis risus. Nunc efficitur ultrices placerat.
+            After the science olympiad, I became friends with one of my
+            competitors at the time, Ádám Vajda. Together, in 2024, we developed
+            the world&apos;s first self-driving wheelchair. We began development
+            in January, achieved fully autonomous indoor navigation by March,
+            patented our technology in April, and continued working on
+            implementing outdoor navigation over the summer.
           </p>
         </div>
       </section>
@@ -267,11 +287,15 @@ export default function Page() {
       <section className="px-4 py-3 overflow-hidden">
         <div className="max-w-2xl mx-auto">
           <p className="text-justify">
-            Avatar - Sed et libero ut velit dapibus facilisis non sit amet
-            augue. Vestibulum quam ligula, fringilla id sapien sed, blandit
-            fringilla mauris. Duis facilisis luctus magna eget venenatis.
-            Integer nec diam suscipit, pretium leo vitae, mattis risus. Nunc
-            efficitur ultrices placerat.
+            Meanwhile, I was also working on another project. At the beginning
+            of the year, I realized that Hungary's education system is short of
+            16,000 teachers and that a reliable and friendly AI model could help
+            solve this problem. This led me to develop Sophie, the world&apos;s
+            first virtual teacher, with whom students could have spoken
+            conversations, and she would respond based on the Hungarian
+            curriculum. Currently, at the time of writing this article, I have
+            helped over 5,000 students and am in negotiations with the Hungarian
+            government regarding a potential acquisition.
           </p>
         </div>
       </section>
