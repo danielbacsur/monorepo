9d7f24e032cdc46caffc9afcdc7b3d6cb1581afe
Daniel Bacsur <contact@danielbacsur.com>
2024-12-21 10:29:42 +0100

    feat: make middleware handle both domains dev and com domains

--- apps/portfolio/middleware.ts
+++ apps/portfolio/middleware.ts
@@ -3,27 +3,35 @@ import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const hostname = request.headers.get("host");
+  const { pathname } = request.nextUrl;
 
-  if (hostname?.includes("danielbacsur.com")) {
-    return NextResponse.redirect(
-      new URL("https://danielbacsur.com/personal-portfolio"),
-      301
-    );
+  if (pathname === "/") {
+    if (hostname?.includes("danielbacsur.com")) {
+      return NextResponse.redirect(
+        new URL("https://danielbacsur.com/personal-portfolio"),
+        301
+      );
+    }
+
+    if (hostname?.includes("danielbacsur.dev")) {
+      return NextResponse.redirect(
+        new URL("https://danielbacsur.com/developer-portfolio"),
+        301
+      );
+    }
   }
 
-  if (hostname?.includes("danielbacsur.dev")) {
-    return NextResponse.redirect(
-      new URL("https://danielbacsur.com/developer-portfolio"),
-      301
-    );
+  if (pathname.startsWith("/projects")) {
+    if (hostname?.endsWith(".dev")) {
+      const newUrl = new URL(request.url);
+      newUrl.host = hostname.replace(".dev", ".com");
+      return NextResponse.redirect(newUrl, 301);
+    }
   }
 
-  return NextResponse.redirect(
-    new URL("/personal-portfolio", request.url),
-    301
-  );
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/",
+  matcher: ["/", "/projects/:path"],
 };
