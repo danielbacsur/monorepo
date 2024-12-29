250fb82882b9dc8a7f0b3c65b4a43fecd0823edb
Daniel Bacsur <contact@danielbacsur.com>
2024-12-29 18:50:35 +0100

    feat: add youtube-video component and remote-mdx support

--- apps/portfolio/app/projects/[id]/page.tsx
+++ apps/portfolio/app/projects/[id]/page.tsx
@@ -5,6 +5,7 @@ import { getContributions } from "@/utils/github";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { Metadata } from "next";
 import { MasonryGallery } from "@/components/masonry-gallery";
+import { YoutubeVideo } from "@/components/youtube-video";
 
 export const revalidate = 3600;
 export const dynamicParams = false;
@@ -81,6 +82,7 @@ export default async function ProjectPage({
             source={project.content}
             components={{
               MasonryGallery,
+              YoutubeVideo,
             }}
           />
         </section>
---
+++ apps/portfolio/components/youtube-video.tsx
@@ -0,0 +1,11 @@
+export function YoutubeVideo({ src, title }: { src: string; title: string }) {
+  return (
+    <iframe
+      src={src}
+      title={title}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    />
+  );
+}
