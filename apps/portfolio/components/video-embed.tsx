bbbb6ad3871d929a33c7bfbc0c633409f4c5e55a
Daniel Bacsur <contact@danielbacsur.com>
2024-12-31 15:49:30 +0100

    feat: style video-embed component with custom classes

--- apps/portfolio/components/video-embed.tsx
+++ apps/portfolio/components/video-embed.tsx
@@ -3,6 +3,7 @@ export function VideoEmbed({ src, title }: { src: string; title: string }) {
     <iframe
       src={src}
       title={title}
+      className="w-full my-8 aspect-video rounded-md"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       referrerPolicy="strict-origin-when-cross-origin"
       allowFullScreen
